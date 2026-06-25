b08cb22288cb21f4911f6567c8d06696578a4a77
gayaneditz-bit <gayaneditz@gmail.com>
2026-06-25 17:59:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
+SESSION_ID: process.env.SESSION_ID || "HASI-MD=kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
